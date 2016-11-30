7ad2a70d2c314495c47394e5c5013e54a543c303
awang1 <awang1@andrew.cmu.edu>
2016-11-30 16:57:20 -0500

    Finalize postgres installation

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161121212620) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reservations", force: :cascade do |t|
     t.integer  "party_size"
     t.datetime "time_reserved"
@@ -22,8 +25,8 @@ ActiveRecord::Schema.define(version: 20161121212620) do
     t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id"
-    t.index ["user_id"], name: "index_reservations_on_user_id"
+    t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id", using: :btree
+    t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -51,4 +54,6 @@ ActiveRecord::Schema.define(version: 20161121212620) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "restaurants"
+  add_foreign_key "reservations", "users"
 end
