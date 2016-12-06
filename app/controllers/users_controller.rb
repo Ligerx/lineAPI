fa7fa2698b698fa2798ebab41d672bfddb842839
awang1 <awang1@andrew.cmu.edu>
2016-12-06 18:14:22 -0500

    Attempt to fix the infinite loop happening before

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -125,12 +125,12 @@ class UsersController < ApplicationController
     def general_estimated_seating_time(restaurant)
       # .count is index + 1, which works because we're looking
       # from the point of view of a reservation that hasn't been created yet
-      next_spot_in_line = restaurant.reservations.waiting.count
+      next_spot_on_wl = restaurant.reservations.waiting.count
 
-      # puts '-------- general estimated seating time next_spot_in_line is '
-      # puts next_spot_in_line
-
-      time = estimated_seating_time(restaurant, next_spot_in_line)
+      # puts '-------- general estimated seating time next_spot_on_wl is '
+      # puts next_spot_on_wl
+      # byebug
+      time = estimated_seating_time(restaurant, next_spot_on_wl)
 
       time.strftime('%Y/%m/%d %H:%M')
     end
@@ -145,51 +145,49 @@ class UsersController < ApplicationController
       return nil if user_reservation.nil?
 
       waiting_list = restaurant.reservations.waiting.by_time_reserved
-      user_spot_in_line = find_spot_in_line(user_reservation, waiting_list)
+      user_spot_on_wl = find_spot_in_line(user_reservation, waiting_list)
 
-      # puts '-------- personal estimated seating time user_spot_in_line is '
-      # puts user_spot_in_line
+      # puts '-------- personal estimated seating time user_spot_on_wl is '
+      # puts user_spot_on_wl
 
-      time = estimated_seating_time(restaurant, user_spot_in_line)
+      time = estimated_seating_time(restaurant, user_spot_on_wl)
 
       time.strftime('%Y/%m/%d %H:%M')
     end
     helper_method :personal_estimated_seating_time
 
-    def estimated_seating_time(restaurant, spot_in_line)
+    def estimated_seating_time(restaurant, spot_on_wl)
       num_empty_tables = restaurant.num_tables - restaurant.reservations.still_eating.count
       seated_and_waiting_list = restaurant.reservations.waiting_or_seated.by_time_reserved.by_time_seated
 
-      calculate_estimated_seating_time(spot_in_line, num_empty_tables, seated_and_waiting_list)
+      waiting_list = restaurant.reservations.waiting.by_time_reserved
+
+      calculate_estimated_seating_time(spot_on_wl, num_empty_tables, seated_and_waiting_list, waiting_list)
     end
 
     # Calculate estimated seating time based on the corresponding person in front of them.
     # This could be a seated table or someone waiting in line.
     # If it's someone waiting, calculate the time based on who THAT person is waiting for.
-    def calculate_estimated_seating_time(spot_in_line, num_empty_tables, seated_and_waiting_list)
+    def calculate_estimated_seating_time(spot_on_wl, num_empty_tables, seated_and_waiting_list, waiting_list)
       # Table available immediately if you're in the front of the line and a table is open for you.
 
-      # puts '-------- inside calculate estimated seating time ------------'
-      # puts spot_in_line
-      # puts num_empty_tables
-
-      empty_table_available_for_user = spot_in_line < num_empty_tables
+      empty_table_available_for_user = spot_on_wl < num_empty_tables
       return DateTime.now if empty_table_available_for_user
 
-      corresponding_spot_in_line = spot_in_line - num_empty_tables
-      corresponding_reservation = seated_and_waiting_list[corresponding_spot_in_line]
+      corresponding_reservation = seated_and_waiting_list[spot_on_wl - num_empty_tables]
 
       if corresponding_reservation.is_seated?
         return corresponding_reservation.time_seated + 1.hour
       else
         # Look at who the person you're waiting for is waiting for [recursive]
-        return calculate_estimated_seating_time(corresponding_spot_in_line, num_empty_tables, seating_and_waiting_list) + 1.hour
+        corresponding_spot_in_wl = find_spot_in_line(corresponding_reservation, waiting_list)
+        return calculate_estimated_seating_time(corresponding_spot_in_wl, num_empty_tables, seated_and_waiting_list, waiting_list) + 1.hour
       end
 
     end
 
-    def find_spot_in_line(reservation, waiting_list)
+    def find_spot_in_line(reservation, list)
       # Return the index of the reservation in the waiting list
-      waiting_list.find_index(reservation) # remember, counting starts at 0
+      list.find_index(reservation) # remember, counting starts at 0
     end
 end
