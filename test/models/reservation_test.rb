edf4816ba3c7df4a719f061394bbe3fb1a637fd7
awang1 <awang1@andrew.cmu.edu>
2016-12-08 03:43:05 -0500

    Update fixture data

--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -8,5 +8,15 @@ class ReservationTest < ActiveSupport::TestCase
   should validate_presence_of(:restaurant)
   should validate_presence_of(:user)
 
-  
+  test "default values" do
+    reservation = Reservation.new
+    reservation.user = users(:one)
+    reservation.restaurant = restaurants(:one)
+    reservation.party_size = 2
+
+    reservation.save
+
+    assert_not reservation.cancelled
+    assert_in_delta reservation.time_reserved, DateTime.now, 1.second
+  end
 end
