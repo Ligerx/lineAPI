22d291654a61eb8c2e18ba0c0ebf8b9a97a25ef9
awang1 <awang1@andrew.cmu.edu>
2016-12-08 05:16:25 -0500

    jk renamed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/users/:id/restaurants', to: 'users#restaurants', as: :userRestaurants
-  get '/users/:id/restaurants/:restaurant_id', to: 'users#restaurant', as: :userRestaurant
-  post '/users/:id/restaurants/:restaurant_id/make-reservation', to: 'users#make_reservation', as: :makeReservation
-  post '/users/:id/restaurants/cancel-reservation', to: 'users#cancel_reservation', as: :cancelReservation
+  get '/users/:id/restaurants', to: 'users#restaurants', as: :user_restaurants
+  get '/users/:id/restaurants/:restaurant_id', to: 'users#restaurant', as: :user_restaurant
+  post '/users/:id/restaurants/:restaurant_id/make-reservation', to: 'users#make_reservation', as: :make_reservation
+  post '/users/:id/restaurants/cancel-reservation', to: 'users#cancel_reservation', as: :cancel_reservation
 
   get'/web', to: 'users#web'
   root 'users#web'
