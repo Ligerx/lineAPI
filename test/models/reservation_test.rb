259e3a274d5ab21e0ac16f43dc557455eaaff0d1
awang1 <awang1@andrew.cmu.edu>
2016-12-08 03:12:01 -0500

    Install shoulda matchers for testing

--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:restaurant)
+  should belong_to(:user)
+
+  should validate_presence_of(:party_size)
+  should validate_presence_of(:restaurant)
+  should validate_presence_of(:user)
+
+  
 end
