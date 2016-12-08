979bf8ef90a83ac422c7c1daaee775f73c1391e1
awang1 <awang1@andrew.cmu.edu>
2016-12-08 09:14:09 -0500

    Complete like 90% of tests, good enough for now

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -71,30 +71,70 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "general time with an open table" do
+    user4 = users(:user4)
+    unionGrill = restaurants(:unionGrill)
 
+    get user_restaurant_url(user4, unionGrill), as: :json
+    json = JSON.parse(response.body)
+
+    assert_in_delta DateTime.now, DateTime.parse(json['restaurant']['general_estimated_seating_time']), 1.second
   end
 
   test "general time with no open tables and short line" do
+    userOne = users(:one)
+    unionGrill = restaurants(:unionGrill)
+    # Make a reservation first
+    post make_reservation_url(userOne, unionGrill), params: { reservation: { party_size: 5 } }, as: :json
 
+    get user_restaurant_url(userOne, unionGrill), as: :json
+    json = JSON.parse(response.body)
+
+    # this date string is coming from the fixtures for the first seated person at Union Grill.
+    # I just copied it over for this
+    assert_in_delta DateTime.parse("2016-11-21 17:00:00") + 1.hour, DateTime.parse(json['restaurant']['general_estimated_seating_time']), 1.second
   end
 
   test "general time with long line" do
+    # With 2 tables and 1 being seated in the fixture, I need to add 3 more people
+    # in line to test the recursive capabilities of finding time
+    unionGrill = restaurants(:unionGrill)
+    user4 = users(:user4)
+    userOne = users(:one)
+    userTwo = users(:two)
+
+    post make_reservation_url(user4, unionGrill), params: { reservation: { party_size: 5 } }, as: :json
+    post make_reservation_url(userOne, unionGrill), params: { reservation: { party_size: 5 } }, as: :json
+    post make_reservation_url(userTwo, unionGrill), params: { reservation: { party_size: 5 } }, as: :json
+
+    get user_restaurant_url(userOne, unionGrill), as: :json
+    json = JSON.parse(response.body)
+
+    # this date string is coming from the fixtures for the first seated person at Union Grill.
+    # I just copied it over for this
+    assert_in_delta DateTime.parse("2016-11-21 17:00:00") + 2.hour, DateTime.parse(json['restaurant']['general_estimated_seating_time']), 1.second
 
   end
 
   test "personal time with an open table" do
-
+    skip 'Skipping personal time tests because they share very similar code to the general time estimates.'
   end
 
   test "personal time with no open tables and short line" do
-
+    skip
   end
 
   test "personal time with long line" do
-
+    skip
   end
 
   test "position in line should be null if not in line" do
+    unionGrill = restaurants(:unionGrill)
+    user4 = users(:user4)
+
+    get user_restaurant_url(user4, unionGrill), as: :json
+    json = JSON.parse(response.body)
+
+    assert_nil json['restaurant']['position_in_line']
 
   end
 
