e3e85e666415cbd10e157021f8e7271912dcbb4d
awang1 <awang1@andrew.cmu.edu>
2016-12-08 03:55:27 -0500

    WOrking on tests

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,10 +9,10 @@ class Reservation < ApplicationRecord
 
   scope :not_seated, -> { where(time_seated: nil) }
   scope :seated, -> { where.not(time_seated: nil) }
-  scope :has_left, -> { where.not(time_left: nil) }
+  # scope :has_left, -> { where.not(time_left: nil) }
   scope :hasnt_left, -> { where(time_left: nil) }
   scope :still_eating, -> { seated.where(time_left: nil) }
-  scope :cancelled, -> { where(cancelled: true) }
+  # scope :cancelled, -> { where(cancelled: true) }
   scope :not_cancelled, -> { where.not(cancelled: true) }
   scope :waiting, -> { where.not(time_reserved: nil).not_seated.not_cancelled } # not sure if not including time_left matters
   scope :for_user, ->(user_id) { where(user_id: user_id) }
@@ -21,9 +21,9 @@ class Reservation < ApplicationRecord
   scope :by_time_reserved, -> { order(:time_reserved) }
   scope :by_time_seated, -> { order(:time_seated) }
 
-  def position_in_line
-    # given a user_id?, find the position of that user in the waiting list line
-  end
+  # def position_in_line
+  #   # given a user_id?, find the position of that user in the waiting list line
+  # end
 
   def is_seated?
     # Not sure if I need to check for other conditions other than time_seated
--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -9,14 +9,35 @@ class ReservationTest < ActiveSupport::TestCase
   should validate_presence_of(:user)
 
   test "default values" do
-    reservation = Reservation.new
-    reservation.user = users(:one)
-    reservation.restaurant = restaurants(:one)
-    reservation.party_size = 2
-
+    reservation = buildReservation
     reservation.save
 
     assert_not reservation.cancelled
     assert_in_delta reservation.time_reserved, DateTime.now, 1.second
+  end
+
+  test "not seated scope" do
+    reservation = buildReservation
+    reservation.save
+
+    assert_equal Reservation.not_seated.count, 2
+  end
+
+  test "seated scope" do
+    assert_equal Reservation.not_seated.count, 2
+  end
+
+  test "hasnt left scope" do
+    assert_equal Reservation.hasnt_left.count, 2
+  end
+
+
+  # helper method
+  def buildReservation
+    reservation = Reservation.new
+    reservation.user = users(:user3)
+    reservation.restaurant = restaurants(:unionGrill)
+    reservation.party_size = 2
+    return reservation
   end
 end
