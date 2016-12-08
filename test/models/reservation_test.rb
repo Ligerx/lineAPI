af293ddf54757a3b89e46b8f6647886e815ec3d3
awang1 <awang1@andrew.cmu.edu>
2016-12-08 04:48:56 -0500

    Workin on tests still

--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -16,6 +16,10 @@ class ReservationTest < ActiveSupport::TestCase
     assert_in_delta reservation.time_reserved, DateTime.now, 1.second
   end
 
+  test "seated scope" do
+    assert_equal Reservation.seated.count, 5
+  end
+
   test "not seated scope" do
     reservation = buildReservation
     reservation.save
@@ -23,12 +27,33 @@ class ReservationTest < ActiveSupport::TestCase
     assert_equal Reservation.not_seated.count, 2
   end
 
-  test "seated scope" do
-    assert_equal Reservation.not_seated.count, 2
+  test "hasnt left scope" do
+    assert_equal Reservation.hasnt_left.count, 3
   end
 
-  test "hasnt left scope" do
-    assert_equal Reservation.hasnt_left.count, 2
+  test "not cancelled scope" do
+    reservation = buildReservation
+    reservation.cancelled = true
+    reservation.save
+
+    assert_equal Reservation.not_cancelled.count, Reservation.all.count - 1
+  end
+
+  test "still eating scope" do
+    assert_equal Reservation.still_eating.count, 2
+  end
+
+  test "waiting scope" do
+    assert_equal Reservation.waiting.count, 1
+  end
+
+  test "for user scope" do
+    alex = users(:alex)
+    assert_equal Reservation.for_user(alex.id).count, 1
+
+    user3 = users(:user3)
+    assert_equal Reservation.for_user(user3.id).count, 2
+
   end
 
 
